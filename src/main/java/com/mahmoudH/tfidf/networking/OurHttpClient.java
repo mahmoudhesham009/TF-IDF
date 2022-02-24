e2985dcc596629777c242845b83b6c0344dc097b
mahmoudhesham009 <35175706+mahmoudhesham009@users.noreply.github.com>
2022-02-24 17:35:59 +0200

    commit

--- src/main/java/com/mahmoudH/tfidf/networking/OurHttpClient.java
+++ src/main/java/com/mahmoudH/tfidf/networking/OurHttpClient.java
@@ -1,6 +1,21 @@
 package com.mahmoudH.tfidf.networking;
 
-public class OurHttpClient {
-    
 
+import com.mahmoudH.tfidf.model.Results;
+import com.mahmoudH.tfidf.model.Task;
+
+import java.net.http.HttpClient;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class OurHttpClient {
+    HttpClient client;
+
+    public OurHttpClient() {
+        this.client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
+    }
+
+    CompletableFuture<Results> sendTasks(List<String>workers, List<Task> tasks){
+        
+    }
 }
